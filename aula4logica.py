6a62a4d4803fc4293e4ec4d0d30434d939b4fad8
walterlorenzo <70152476+walterlorenzo@users.noreply.github.com>
2023-05-06 20:31:38 -0300

    Add files via upload

---
+++ aula4logica.py
@@ -0,0 +1,16 @@
+# Coleções(listas)
+preço_1 = 20
+preço_2 = 50
+preço_3 = 200
+# Lista
+preços = [20,50,200]
+#         0, 1 . 2
+print(preços[0])
+print(preços.index(200))
+# Lista
+diversidades = [15, "Jhonatan", True]
+print(diversidades[0])
+print(diversidades[1])
+print(diversidades[2])
+for preço in preços:
+    print(preço)
---
+++ aula4logica2.py
@@ -0,0 +1,17 @@
+# Coleções(listas)
+'''
+# Exemplo 5 - Some os valores
+Dados uma coleção de dados "idades"[15,46,75,34,23].
+imprima na tela a soma destes valores
+
+idades = [15,46,75,34,23]
+total = 0
+loop idade em idades
+    total= total + idade
+print total
+'''
+idades = [15,46,75,34,23]
+total = 0
+for idade in idades:
+    total = total + idade
+print(total)
