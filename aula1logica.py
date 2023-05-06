1076600e78e109c7cfde4c34ba76b8e4f95b8ac3
walterlorenzo <70152476+walterlorenzo@users.noreply.github.com>
2023-05-06 20:54:54 -0300

    Add files via upload

---
+++ aula1logica.py
@@ -0,0 +1,25 @@
+# Variaveis
+
+#Numeros
+internet_speed = 10
+print(internet_speed)
+nota_filme = 8.5 # float
+# valores boleanos
+esta_aberto = True
+# Strings
+nome_do_curso = "Lógica de programação"
+
+'''
+input salario_mensal
+input horas_trabalhadas_por_mes
+valor_hora = salario_mensal / horas_trabalhadas_por_mes
+print valor_hora
+'''
+
+salario_mensal = input("Qual é o seu salário mensal? ")
+horas_trabalhadas_por_mes = input("Quantas horas trabalha por mês?")
+valor_hora = int(salario_mensal) / int(horas_trabalhadas_por_mes)
+print(valor_hora)
+
+
+
